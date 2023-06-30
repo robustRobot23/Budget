7871a2cd90b8a39423503e86aa91bebf008d234b
Hayden Evans <hev23@uclive.ac.nz>
2023-06-30 15:53:55 +1200

    minor changes

--- Account.py
+++ Account.py
@@ -1,13 +1,19 @@
-names = set()
-
 class Account:
     def __init__(self, name, startingValue):
         self.balance = startingValue
         self.name = name
-        names.add(name)
+        self.history = [startingValue]
+        # self.log = {}
+        
 
     def transact(self, transactAmount):
         if self.balance + transactAmount < 0:
             raise Exception("Tried to withdraw too much")
         else:
             self.balance += transactAmount
+
+        print(f"{self.name} transacted ${transactAmount}")
+        # self.log.update({description,transactAmount})
+
+    def update(self):
+        self.history.append(self.balance)
--- main.py
+++ main.py
@@ -1,64 +1,17 @@
-import Account
 import personal
+Bank =  personal.MyBank
+week = 1
+weeks = 25
 
-   
-def requestDeposits():
-    print("Transactions:")
-    done = False
-    doneInput = input("Finished for the week? y/n ")
-    done = doneInput == "y"
-    while (not done):
+while(week <= weeks):
+    print("Week", week)
 
-        inputAccount = ""
-        transactionAmount = 0
-        account = input("Enter Account to Transact to: ")
-        if account in Account.names:
-            currentAccount = personal.accounts[personal.findAccountIndex(inputAccount)]
-            transactionAmount = float(input("Enter Amount"))
-            if (transactionAmount != 0):
-                currentAccount.transact(transactionAmount)
-        else:
-            print("Account name not found")
+    personalPrototype.weeklyTransactions()
+    if week %4 == 0:
+        personalPrototype.monthlyTransactions()
         
-        doneInput = input("Finished for the week? y/n ")
-        done = doneInput == "y"
-        
+    Bank.updateAccountsHistory()    
+    Bank.printBalance()
+    week += 1 
 
-        
-def printBalance():
-    for account in personal.accounts:
-        print(f"    {account.name:<25} ${account.balance:.2f}")
-
-def init():
-    mode = input("Choose Mode: Update, Free Run  ")
-    weeks = int(input("Choose Weeks: "))
-    if mode == "Update":
-        return True, weeks
-    return False, weeks
-
-def run():
-    week = 1
-    weeks = 25
-    updateTransaction = False
-    # updateTransaction, weeks = init()
-    
-    while(week <= weeks):
-
-        print("Week", week)
-
-        personal.weeklyTransactions()
-        if week %4 == 0:
-            personal.monthlyTransactions()
-
-        if updateTransaction:
-            print("Starting Balance:")
-            printBalance()
-            requestDeposits()
-        
-        print("Finishing Balance:")
-        printBalance()
-        print()
-
-        week += 1   
-
-run()
+Bank.graphBalances(weeks)
